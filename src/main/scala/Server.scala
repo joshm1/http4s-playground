809fec1652158a3ae2af1ba91581fb7e3b36548f
Josh McDade <josh.mcdade@spaceiq.com>
2020-05-20 12:16:16 -0400

    different example (broken) trying to use natchez-http4s

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,14 +1,17 @@
+import java.util.concurrent.atomic.AtomicInteger
+
 import cats.data.Kleisli
-import cats.effect.{Bracket, ConcurrentEffect, ExitCode, IO, IOApp, Resource, Sync, Timer}
+import cats.effect.{ Bracket, ConcurrentEffect, ExitCode, IO, IOApp, Resource, Sync, Timer }
 import cats.implicits._
-import com.ovoenergy.effect.natchez.http4s.server.{Configuration, TraceMiddleware}
-import io.jaegertracing.Configuration.{ReporterConfiguration, SamplerConfiguration}
+import com.ovoenergy.effect.natchez.http4s.server.{ Configuration, TraceMiddleware }
+import io.jaegertracing.Configuration.{ ReporterConfiguration, SamplerConfiguration }
 import natchez._
 import natchez.jaeger.Jaeger
 import org.http4s.dsl.Http4sDsl
 import org.http4s.implicits._
+import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.{HttpApp, HttpRoutes}
+import org.http4s.{ HttpApp, HttpRoutes }
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = (new Server).run[IO]
@@ -16,23 +19,25 @@ object Main extends IOApp {
 
 class Server {
 
-  def _routes[F[_] : Sync : Trace]: HttpRoutes[F] = {
+  def _routesWorkingExample[F[_]: Sync]: HttpRoutes[F] = {
     object dsl extends Http4sDsl[F];
     import dsl._
     HttpRoutes.of[F] {
-      case GET -> Root / "hello" / name =>
-        Trace[F].put("woot" -> 42) *>
-          Trace[F].span("responding") {
-            Ok(s"Hello, $name.")
-          }
+      case GET -> Root / "hello" / name => Ok(name)
     }
   }
 
-  def routes[F[_] : Bracket[*[_], Throwable]](
-                                               implicit ev: Sync[Kleisli[F, Span[F], *]])
-  : HttpRoutes[Kleisli[F, Span[F], *]] = _routes
+  def _routesBetterExampleOfRealUseCase[F[_]: Sync](endpoints: Endpoints[F]): HttpRoutes[F] =
+    Router(
+      "/foo" -> endpoints.foo.service,
+      "/bar" -> endpoints.bar.service,
+    )
 
-  def entryPointResource[F[_] : Sync]: Resource[F, EntryPoint[F]] =
+  def routes[F[_]: Bracket[*[_], Throwable]](endpoints: Endpoints[F])(
+      implicit ev: Sync[Kleisli[F, Span[F], *]]
+  ): HttpRoutes[Kleisli[F, Span[F], *]] = _routesBetterExampleOfRealUseCase(endpoints)
+
+  def entryPointResource[F[_]: Sync]: Resource[F, EntryPoint[F]] =
     Jaeger.entryPoint[F]("natchez-example") { c =>
       Sync[F].delay {
         c.withSampler(SamplerConfiguration.fromEnv)
@@ -41,22 +46,69 @@ class Server {
       }
     }
 
-  def configuration[F[_] : Sync]: Configuration[F] = Configuration.default()
+  def configuration[F[_]: Sync]: Configuration[F] = Configuration.default()
 
-  def app[F[_] : Sync : Bracket[*[_], Throwable]](ep: EntryPoint[F]): HttpApp[F] = {
-    val baseApp = routes.orNotFound
+  // dummy transactor (in real code, this is from doobie)
+  def getTransactor[F[_]: Sync]: Resource[F, Transactor[F]] =
+    Resource.make(Sync[F].pure(new Transactor[F] {}))(_ => Sync[F].unit)
+
+  def getEndpoints[F[_]: Sync](xa: Transactor[F]): Endpoints[F] =
+    new Endpoints[F](
+      new FooHttpEndpoint[F](xa),
+      new BarHttpEndpoint[F](xa)
+    )
+
+  def app[F[_]: Sync: Bracket[*[_], Throwable]](ep: EntryPoint[F], xa: Transactor[F]): HttpApp[F] = {
+    val endpoints = getEndpoints[F](xa)
+    val baseApp = routes(endpoints).orNotFound
     val traceMiddleware: HttpApp[Kleisli[F, Span[F], *]] => HttpApp[F] = TraceMiddleware[F](ep, configuration[F])
     traceMiddleware(baseApp)
   }
 
-  def server[F[_] : ConcurrentEffect : Timer](app: HttpApp[F]): fs2.Stream[F, ExitCode] =
+  def server[F[_]: ConcurrentEffect: Timer](app: HttpApp[F]): fs2.Stream[F, ExitCode] =
     BlazeServerBuilder[F].bindHttp(8888, "0.0.0.0").withHttpApp(app).serve
 
-  def run[F[_] : ConcurrentEffect : Bracket[*[_], Throwable] : Timer]
-  : F[ExitCode] = {
+  def run[F[_]: ConcurrentEffect: Bracket[*[_], Throwable]: Timer]: F[ExitCode] = {
     for {
       entryPoint <- fs2.Stream.resource(entryPointResource)
-      exitCode <- server[F](app(entryPoint))
+      xa <- fs2.Stream.resource(getTransactor[F])
+      exitCode <- server[F](app(entryPoint, xa))
     } yield exitCode
-    }.compile.lastOrError
+  }.compile.lastOrError
+}
+
+trait Transactor[F[_]] {
+  def test(implicit ev: Sync[F]): F[Int] = Sync[F].pure(1)
+}
+
+class Endpoints[F[_]](
+    val foo: FooHttpEndpoint[F],
+    val bar: BarHttpEndpoint[F]
+)
+
+class FooHttpEndpoint[F[_]: Sync](xa: Transactor[F]) extends Http4sDsl[F] {
+
+  val service: HttpRoutes[F] = HttpRoutes.of[F] {
+    case GET -> / => xa.test.flatMap(i => Ok(s"foo ${i}"))
+  }
+}
+
+// this may work, but would require a ton of work across the applications to change the way dependencies
+// are injected across all of my classes (hoping not to have to do this initially), and is a bit messier
+class DifferentFooHttpEndpoint {
+
+  def service[F[_]: Sync](xa: Transactor[F]): HttpRoutes[F] = {
+    object dsl extends Http4sDsl[F]
+    import dsl._
+    HttpRoutes.of[F] {
+      case GET -> / => xa.test.flatMap(i => Ok(s"fixed foo ${i}"))
+    }
+  }
+}
+
+class BarHttpEndpoint[F[_]: Sync](xa: Transactor[F]) extends Http4sDsl[F] {
+
+  val service: HttpRoutes[F] = HttpRoutes.of[F] {
+    case GET -> / => xa.test.flatMap(i => Ok(s"bar ${i}"))
+  }
 }
